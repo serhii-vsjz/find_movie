82d0c51351a09d56352412cf8e0ef4bb4d562e9c
Serhii-Vasyliev-k <vasiliev.ck@gmail.com>
2019-12-02 22:11:02 +0200

    create service param

--- app/Services/FilmService.php
+++ app/Services/FilmService.php
@@ -3,9 +3,61 @@
 
 namespace App\Services;
 
-use App\FilmServiceInterface;
+use App\Film;
+use GuzzleHttp\Client;
+use http\Env\Response;
 
 class FilmService implements FilmServiceInterface
 {
+    /** @var Client $httpClient */
+    private $httpClient;
 
+    public function __construct()
+    {
+        $this->httpClient = new Client([
+            'base_uri' => 'http://www.omdbapi.com'
+        ]);
+    }
+
+    /**
+     * Get one film
+     * @param string $title
+     * @return Film
+     */
+    public function getFilm(string $title): Film
+    {
+        $film = Film::where('title', 'like', '%' . $title . '%')->first();
+        if ($film)
+        {
+            return $film;
+        }
+        $apiFilm = $this->getApiFilm($title);
+        dd($apiFilm);
+    }
+
+    /**
+     * @param string $title
+     * @return array
+     * @throws \Exception
+     */
+
+    private function getApiFilm(string $title): array
+    {
+        $request = $this->httpClient->get(
+            '',
+            [
+                'query' => [
+                    'apikey' =>  config('app.omdb_api_key'),
+                    't' => $title
+                ]
+            ]
+        );
+
+        if ($request->getStatusCode() == Response::HTTP_OK) {
+            throw new \Exception('Film not found');
+        }
+        $response = $request->getBody()->getContents();
+        return json_decode($response, true);
+    }
 }
+
--- app/Services/FilmServiceInterface.php
+++ app/Services/FilmServiceInterface.php
@@ -3,8 +3,16 @@
 
 namespace App\Services;
 
+use App\Film;
 
 interface FilmServiceInterface
 {
-    public function getFilm(string $title);
+    /**
+     * Get one film
+     *
+     * @param string $title
+     * @return Film
+     */
+    public function getFilm(string $title): Film;
 }
+
