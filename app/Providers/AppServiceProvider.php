b8c11ad3bbde2d84a48918543d793083d6fa02e3
Serhii-Vasyliev-k <vasiliev.ck@gmail.com>
2019-12-02 21:08:52 +0200

    Added services film

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Services\FilmService;
+use App\Services\FilmServiceInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(
+            FilmServiceInterface::class,
+            FilmService::class
+        );
     }
 
     /**
---
+++ app/Services/FilmService.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Services;
+
+
+class FilmService implements FilmServiceInterface
+{
+
+}
