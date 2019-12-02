3497027d8de5ec549f205af6af3e68e38786f8f7
Serhii-Vasyliev-k <vasiliev.ck@gmail.com>
2019-12-02 21:41:19 +0200

    create model and migrate

--- app/Services/FilmService.php
+++ app/Services/FilmService.php
@@ -3,6 +3,7 @@
 
 namespace App\Services;
 
+use App\FilmServiceInterface;
 
 class FilmService implements FilmServiceInterface
 {
---
+++ database/migrations/2019_12_02_191208_create_films_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('year');
+            $table->string('genre');
+            $table->string('director');
+            $table->string('runtime');
+            $table->string('plot');
+            $table->string('poster');
+            $table->string('actors');
+            $table->string('imdb_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('films');
+    }
+}
